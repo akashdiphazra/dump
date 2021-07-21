9940489d97d018b86e9597d8e7a8ea19e4fa45ac
Akashdip Hazra <61003681+akashdiphazra@users.noreply.github.com>
2021-07-21 22:11:25 +0530

    Update PassingArraysUsingFunctions.cpp

--- C++/Tutorials/Arrays/PassingArraysUsingFunctions.cpp
+++ C++/Tutorials/Arrays/PassingArraysUsingFunctions.cpp
@@ -6,17 +6,28 @@ void printarray2(int *arr, int size);
 
 int main()
 {
-    int akash[4] = {1, 33, 44, 55};
-    int ravi[6] = {44, 55, 66, 73, 25, 8};
-    printarray(akash, 4);
-    printarray(ravi, 6);
+    int akash[6] = {123, 33, 44, 55, 45, 134};
+    int ravi[6] = {44, 55, 66, 73, 25, 823};
+    printarray1(akash, 6);
+    cout << endl;
+    printarray2(ravi, 6);
     return 0;
 }
 
-void printarray(int arr[], int size)
+void printarray1(int arr[], int size)
 {
+    cout << "Using only arrays: " << endl;
     for (int i = 0; i < size; i++)
     {
-        cout << arr[i] << endl;
+        cout << arr[i] << " ";
+    }
+}
+
+void printarray2(int *arr, int size)
+{
+    cout << "Using pointers: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
