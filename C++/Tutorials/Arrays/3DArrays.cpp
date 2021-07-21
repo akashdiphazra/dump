c4d78a18ea33470f3e0fba39f1cff938ad059aea
Akashdip Hazra <61003681+akashdiphazra@users.noreply.github.com>
2021-07-21 21:10:15 +0530

    Update 3DArrays.cpp
    Updated 3D Arrays

--- C++/Tutorials/Arrays/3DArrays.cpp
+++ C++/Tutorials/Arrays/3DArrays.cpp
@@ -23,5 +23,20 @@ int main()
         }
         cout << endl;
     }
+    cout << "3D Representation:\n" << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                cout << "array[" << i << "][" << j << "][" << k << "] = " << array_3d[i][j][k] << "  ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
     return 0;
 }
