43667c37b24d3b15f186259fd41b351b2fae5d9b
Akashdip Hazra <61003681+akashdiphazra@users.noreply.github.com>
2021-07-21 20:35:27 +0530

    Update MultidimensionalArraysAndFunctions.cpp

--- C++/Tutorials/Arrays/MultidimensionalArraysAndFunctions.cpp
+++ C++/Tutorials/Arrays/MultidimensionalArraysAndFunctions.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void funct(int (*a)[2][2]) // (int( *c)[3])
+void funct(int (*array)[2][3]) // (int( *c)[3])
 {
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                cout << array[i][j][k] << " ";
+            }
+        }
+        cout << endl;
+    }
 }
 
 int main()
 {
-    int a[3][2][2] = {{{2, 5}, {7, 9}},
-                      {{3, 4}, {6, 1}},
-                      {{0, 8}, {11, 13}}};
+    int a[3][2][3] = {{{2, 5, 2}, {7, 9, 11}},
+                      {{3, 4, 2}, {6, 1, 11}},
+                      {{0, 8, 2}, {11, 13, 23}}};
 
-    int b[2] = {1, 2};
-    int c[2][3] = {{2, 4, 6}, {5, 7, 8}};
+    // int b[2] = {1, 2};
+    // int c[2][3] = {{2, 4, 6}, {5, 7, 8}};
     funct(a);
     return 0;
 }
